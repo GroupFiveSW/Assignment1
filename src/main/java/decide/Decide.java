4b3eb9582d4877fe5096cac704debbf57582df74
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2022-01-27 14:50:22 +0100

    adds root structure for Decide

---
+++ src/main/java/decide/Decide.java
@@ -0,0 +1,22 @@
+package decide;
+
+
+
+
+public class Decide {
+
+    //Constant
+    static double PI = 3.1415926535;
+
+    //Type Declarations
+    enum CONNECTORS {
+        ORR,
+        ANDD
+    }
+
+    static double COORDINATE;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Decide!");
+    }
+}
