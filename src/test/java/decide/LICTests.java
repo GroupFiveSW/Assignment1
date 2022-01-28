1da0a0e86204bc8c39e6a2a8f546a35c0c1a9b15
osp <osp@kth.se>
2022-01-28 18:04:51 +0100

    feat/#33. Create testcase with parameters needed to test LIC functionality with a sequence of planar data points.

--- src/test/java/decide/LICTests.java
+++ src/test/java/decide/LICTests.java
@@ -1,10 +1,57 @@
 package decide;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LICTests {
+
+    Decide D = new Decide();
+    Random rand = new Random();
+    Decide.CONNECTORS orr = Decide.CONNECTORS.ORR;
+    Decide.CONNECTORS andd = Decide.CONNECTORS.ANDD;
+    Decide.CONNECTORS notused = Decide.CONNECTORS.NOTUSED;
+
+
+    @BeforeEach
+    void setUp(){
+        for(int i=0; i<100; i++){
+
+            D.X[i] = (rand.nextDouble()*200)-100;
+            D.X2[i] = D.X[i];
+
+            D.Y[i] = (rand.nextDouble()*200)-100;
+            D.Y2[i] = D.Y[i];
+        }
+
+        D.NUMPOINTS = rand.nextInt(60) +40;
+
+
+        for(int i = 0; i<15; i++){
+            for(int j = i; j<15;j++){
+                int r = rand.nextInt(2);
+                if(r==0){
+                    D.LCM[i][j] = orr;
+                    D.LCM[j][i] = orr;
+                }
+                else if (r==1){
+                    D.LCM[i][j] = andd;
+                    D.LCM[j][i] = andd;
+                }
+                else if (r==2){
+                    D.LCM[i][j] = notused;
+                    D.LCM[j][i] = notused;
+                }
+            }
+        }
+
+    }
+
+    /**
+     *
+     */
 
     /**
      * Tests LIC 4 with 2 points in 2 quadrants, requiring more than 1 quadrant.
