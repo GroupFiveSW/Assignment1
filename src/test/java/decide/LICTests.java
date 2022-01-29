15531c524430795ea44571f1d2831f8651169efd
elibon99 <elias.bonnici@gmail.com>
2022-01-29 10:40:01 +0100

    feat/#5. Implmented LIC2 with unit tests

--- src/test/java/decide/LICTests.java
+++ src/test/java/decide/LICTests.java
@@ -7,6 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class LICTests {
 
     /**
+     * Test LIC 2 with 3 points that form an angle of 90 degrees.
+     * As such LIC 2 should be satisfied since 90 degrees is > PI+EPSILON
+     */
+    @Test
+    void condition2Satisfied() {
+        LIC lic = new LIC();
+
+        Decide.X2 = new double[]{0,0,1};
+        Decide.Y2 = new double[]{1,0,0};
+        Decide.PARAMETERS2.EPSILON = Math.pow(10,-3);
+        Decide.NUMPOINTS2 = 3;
+
+        lic.condition2();
+
+        assertEquals(true, Decide.CMV2[2]);
+    }
+
+    /**
+     * Test LIC 2 with three points in which the last point coincides with vertex.
+     * As such LIC 2 should not be satisfied.
+     */
+    @Test
+    void condition2NotSatisfied() {
+        LIC lic = new LIC();
+
+        Decide.X2 = new double[] {0,1,1};
+        Decide.Y2 = new double[] {0,0,0};
+        Decide.PARAMETERS2.EPSILON = Math.pow(10, -3);
+        Decide.NUMPOINTS2 = 3;
+
+        lic.condition2();
+        assertFalse(Decide.CMV2[2]);
+    }
+
+
+    /**
      * Tests LIC 4 with 2 points in 2 quadrants, requiring more than 1 quadrant.
      * 2 > 1 meaning it should be satisfied.
      */
@@ -37,6 +73,7 @@ class LICTests {
         Decide.PARAMETERS2.QUADS = 1;
         Decide.X2 = new double[]{0, -1};
         Decide.Y2 = new double[]{-2, -1};
+        Decide.NUMPOINTS2 = 2;
 
         lic.condition4();
 
