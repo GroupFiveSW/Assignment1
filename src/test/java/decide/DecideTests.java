33c5bee728587bd8c2184ea2573ce47f9c0dfb4c
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2022-01-31 17:57:38 +0100

    feat/#53. Adds test structure for Decide function

--- src/test/java/decide/DecideTests.java
+++ src/test/java/decide/DecideTests.java
@@ -1,2 +1,27 @@
-package decide;public class DecideTests {
+package decide;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DecideTests {
+
+    HelperFunctions helper = new HelperFunctions();
+    LIC lic = new LIC();
+
+    @Test
+    void launchFalseTest(){
+
+        //@TODO: Implement testing function once Decide-function is finished
+        Decide.PARAMETERS_T params = new Decide.PARAMETERS_T(
+                1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1,
+                1, 1, 1);
+
+
+    }
+
+
 }
