969b5c6d8154c1ce6254d4c375aeb019e1dee28c
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2022-02-02 13:00:48 +0100

    feat. Adds more unit tests for Decide program.

--- src/test/java/decide/DecideTests.java
+++ src/test/java/decide/DecideTests.java
@@ -14,7 +14,6 @@ class DecideTests {
     @Test
     void launchTrueTest1(){
 
-        //@TODO: Implement testing function once Decide-function is finished
         Decide.PARAMETERS2 = new Decide.PARAMETERS_T(
                 3, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1,
@@ -62,6 +61,59 @@ class DecideTests {
         assertFalse(Decide.CMV2[1]);
         assertFalse(Decide.CMV2[2]);
         assertTrue(Decide.LAUNCH2);
+    }
+
+    /** Test Decide with LIC 0,1,2 being false but decide launches because PUV are all false */
+    @Test
+    void launchFalseTest(){
+        Decide.PARAMETERS2 = new Decide.PARAMETERS_T(
+                8, 10, Decide.PI*0.51, 1, 1,
+                1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1,
+                1, 1, 1);
+        Decide.NUMPOINTS2 = 3;
+        Decide.X2 = new double[]{0,0,4};
+        Decide.Y2 = new double[]{4,0,0};
+        Decide.PUV = new boolean[15];
+        Decide.CONNECTORS[] conns = {Decide.CONNECTORS.ANDD, Decide.CONNECTORS.ORR, Decide.CONNECTORS.NOTUSED};
+        for(int i = 0; i<15; i++){
+            if(i < 4) {
+                Decide.PUV[i] = true;
+            }
+            for(int j = i; j<15; j++){
+
+                Decide.LCM2[i][j] = conns[0];
+            }
+        }
+        Decide.decide();
+        assertFalse(Decide.CMV2[0]);
+        assertFalse(Decide.CMV2[1]);
+        assertFalse(Decide.CMV2[2]);
+        assertFalse(Decide.LAUNCH2);
+    }
+
+    /** Test Decide with LIC 0,1,2 being false but decide launches because PUV are all false */
+    @Test
+    void launchFalseInvalidInputTest(){
+        Decide.PARAMETERS2 = new Decide.PARAMETERS_T(
+                8, 10, Decide.PI*0.51, 1, 1,
+                1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1,
+                1, 1, 1);
+
+        Decide.X2 = new double[]{0,0};
+        Decide.Y2 = new double[]{4,0};
+        Decide.NUMPOINTS2 = Decide.X2.length;
+        Decide.PUV = new boolean[15];
+        Decide.PUV[6] = true;
+        Decide.CONNECTORS[] conns = {Decide.CONNECTORS.ANDD, Decide.CONNECTORS.ORR, Decide.CONNECTORS.NOTUSED};
+        for(int i = 0; i<15; i++){
+            for(int j = i; j<15; j++){
+                Decide.LCM2[i][j] = conns[0];
+            }
+        }
+        Decide.decide();
+        assertFalse(Decide.LAUNCH2);
     }
 
 
